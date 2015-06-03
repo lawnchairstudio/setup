5aaa6740135069251c28b1c45de0cb41a0bb773e
Patrick Burtchaell <patrick@pburtchaell.com>
2015-06-03 00:16:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Setup
+
+## Install
+
+```
+curl -L https://raw.github.com/lawnchairstudios/setup/master/setup.sh | sh
+```
+
+---
+Copyright 2015 Lawn Chair Studios. All rights reserved.
+
+Licensed MIT.
---
+++ setup.sh
@@ -0,0 +1,20 @@
+##
+# Set up the Lawn Chair Studios development environment
+##
+
+# Exit if any subcommand fails
+set -e
+
+# Install Flow, Node, and nvm
+echo "Installing Homebrew packages..."
+brew install flow
+
+# If Ataom Editor is installed, then install Atom Editor packages
+echo "Installing Atom Editor packages..."
+apm install linter linter-eslint
+
+# Install npm dependencies
+echo "Installing npm dependencies..."
+sudo npm i -g eslint karma webpack
+
+# MTFBWY.
